dc96efa7d7f84d0078f244f19b4f20ae615d0db2
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-04 21:36:09 +0200

    Hello world
    .

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <div className="flex justify-center items-center bg-gray-100 min-h-screen text-gray-800">        
-        <h1 className=''>Hello World!</h1>
+        <h1 className=''>Hello World!!</h1>
     </div>
     </>
   )
