0a9ba4229f57c6669c3ed349ed9900279d765a1c
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-09-05 17:46:10 +0200

    Centered content.

--- src/App.jsx
+++ src/App.jsx
@@ -20,24 +20,26 @@ function App() {
 return (
 	<>
 		<div className="flex items-center justify-center w-screen h-screen">
-			<div className="bg-gray-100 w-[80%] h-[80%] text-gray-800">        
-				<h1 className=''>Hello World!!</h1>
-				<input
-				 	type="text" 
-					className="bg-blue-500" 
-					value={input}
-					onChange={(e) => setInput(e.target.value)}
-				/>
-				<button 
-					className="bg-amber-900 cursor-pointer"
-					onClick={() => {addToDo()}}
-				>
-					Hello
-				</button>
+			<div className="bg-gray-100 w-[40%] h-[80%] text-gray-800">        
+				<h1 className='text-center'>Hello World!!</h1>
+				<div className="flex justify-center gap-1.5">
+					<input
+						type="text" 
+						className="bg-blue-500" 
+						value={input}
+						onChange={(e) => setInput(e.target.value)}
+					/>
+					<button 
+						className="bg-amber-900 cursor-pointer"
+						onClick={() => {addToDo()}}
+					>
+						Hello
+					</button>
+				</div>
 				<ul>
 					{
 						toDos.map((todo, index) => 
-							<div className="flex">
+							<div className="flex justify-center gap-4">
 								<li key={index}>{todo}</li> <input type="checkbox" />
 							</div>
 						)
